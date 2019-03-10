ff484e36db5fd34f458d7b164af80efa67575949
gmoney1289 <47949449+gmoney1289@users.noreply.github.com>
2019-03-09 21:00:50 -0500

    Best Friend Test

---
+++ friend.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var name = prompt("Hi, what is your name?");
+alert("Hello, " + name);
+
+var friend = prompt("Who is your best friend?");
+
+    while (friend != "Greg") {
+    alert("Try Again");
+    var friend = prompt("Who is your best friend?");
+  } alert("Good job, " + name);
+}
