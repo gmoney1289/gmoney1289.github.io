d3ab597b267c460adb09563e6d610346abbc63cd
gmoney1289 <47949449+gmoney1289@users.noreply.github.com>
2019-03-09 20:37:24 -0500

    Best Friend Test

---
+++ Learning.js
@@ -0,0 +1,15 @@
+var grade = "Premium";
+
+switch(grade) {
+    case "Regular":
+        alert("It's $3.15");
+        break;
+    case "Premium":
+        alert("It's $3.35");
+        break;
+    case "Diesel":
+        alert("It's $3.47");
+        break;
+    default:
+        alert("That's not a valid grade.");
+}
---
+++ Untitled-1.js
@@ -0,0 +1,41 @@
+var name = prompt("What is your name?");
+alert("Hello, " + name);
+
+var friend = prompt("Who is your best friend?");
+
+if (friend == "Greg") {
+    alert("Good Job, " + name)
+  } else { 
+    alert("Wrong, " + name)
+    var friend = prompt("Who is your best friend?");
+
+    if (friend == "Greg") {
+        alert("Good Job, " + name)
+      } else { 
+        alert("Wrong, " + name)
+        var friend = prompt("Who is your best friend?");
+
+        if (friend == "Greg") {
+            alert("Good Job, " + name)
+          } else { 
+            alert("Wrong, " + name)
+            var friend = prompt("Who is your best friend?");
+        
+            if (friend == "Greg") {
+                alert("Good Job, " + name)
+              } else { 
+                alert("Wrong, " + name)
+            
+                
+              };
+        
+          };
+    
+        
+      };
+
+  };
+
+
+
+
